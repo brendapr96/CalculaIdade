3afffb162b192687b2509bc94376d9d8b989aa60
brendapr96 <romeiro.brenda96@gmail.com>
2023-09-02 19:51:46 -0300

    PrimeiraVersao

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace pooCalculaIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula a idade de uma pessoa");
+            Pessoa p = new Pessoa();
+            Console.WriteLine("Nome da pessoa: ");
+            p.Nome = Console.ReadLine();
+            Console.WriteLine("Ano de nascimento: ");
+            p.AnoNascimento = Convert.ToInt32(Console.ReadLine());
+            p.ExibirDados();
+
+        }
+    }
+}
